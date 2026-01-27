aaf2c27b98f0b6924e4d6b7737dcbb8c6a5ec2d6
Vantrapati, Vasu Yadav <vasu.vantrapati@worldline.com>
2026-01-27 17:28:44 +0530

    new

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -17,6 +17,9 @@ namespace GCP_Pratice.Controllers
         }
 
         // GET: api/todos
+        /// <summary>
+        /// Retrieves all todos.
+        /// </summary>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
         {
@@ -24,6 +27,9 @@ namespace GCP_Pratice.Controllers
         }
 
         // POST: api/todos
+        /// <summary>
+        /// Creates a new todo.
+        /// </summary>
         [HttpPost]
         public async Task<ActionResult<Todo>> CreateTodo(Todo todo)
         {
@@ -39,12 +45,72 @@ namespace GCP_Pratice.Controllers
         }
 
         // GET: api/todos/1
+        /// <summary>
+        /// Retrieves a specific todo by id.
+        /// </summary>
         [HttpGet("{id}")]
         public async Task<ActionResult<Todo>> GetTodo(int id)
         {
             var todo = await _context.Todos.FindAsync(id);
             if (todo == null) return NotFound();
             return todo;
+        }
+
+        // PUT: api/todos/1
+        /// <summary>
+        /// Updates a specific todo.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="todo"></param>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTodo(int id, Todo todo)
+        {
+            if (id != todo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await TodoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/todos/1
+        /// <summary>
+        /// Deletes a specific todo.
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null) return NotFound();
+
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private async Task<bool> TodoExists(int id)
+        {
+            return await _context.Todos.AnyAsync(e => e.Id == id);
         }
     }
 }
